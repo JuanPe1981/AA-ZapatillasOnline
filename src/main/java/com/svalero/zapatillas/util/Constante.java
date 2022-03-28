b0c26db55576be2105502b89654dda4654b9d065
JuanPe1981 <juanpe981@me.com>
2022-03-28 15:40:45 +0200

    Conectada con BD

--- src/main/java/com/svalero/zapatillas/util/Constante.java
+++ src/main/java/com/svalero/zapatillas/util/Constante.java
@@ -6,7 +6,7 @@ public class Constante {
 
     public static final String ORACLE_URL = "jdbc:oracle:thin:@//localhost:1521/xe";
 
-    public static final String USERNAME = "zapatillaOnlineUsuario";
-    public static final String PASSWORD = "zapatillaOnline1234";
+    public static final String USERNAME = "zapatillasOnlineUsuario";
+    public static final String PASSWORD = "zapatillasOnline1234";
 
 }
